289570dbf949bea8765f591aa543a2395e71a800
Sova171 <vladkuzak@gmail.com>
2022-11-18 12:13:08 +0200

    rubocop fix

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Post.delete_all
 
 100.times do |i|
   post = Post.create!(title: "Title #{i}", content: "Content #{i}")
   25.times do |j|
-    Comment.create!(post: post, author: "Author #{j}", body: "Comment #{j}")
+    Comment.create!(post:, author: "Author #{j}", body: "Comment #{j}")
   end
 end
